baae708b3f22d17b7a8d95de91c5ed11b24108ef
Matheus <64253249+mattdaspy@users.noreply.github.com>
2023-06-20 13:32:27 +0200

    testing close after sending submit

--- js/sendModal.js
+++ js/sendModal.js
@@ -1,21 +1,25 @@
-document
-          .getElementById("sendEmail")
-          .addEventListener("submit", function (event) {
-            event.preventDefault();
-            document.getElementById("authentication-modal").setAttribute('data-modal-hide', 'true');
-  
-            const serviceID = "service_yobjepi";
-            const templateID = "template_5gsql5o";
-  
-            emailjs.sendForm(serviceID, templateID, this).then(
-              (response) => {
-                console.log("SUCCESS!", response.status, response.text);
-                alert("Email sent, thank you for your contact!");
-              },
-              (error) => {
-                console.log("FAILED...", error);
-                alert("Email failed, please try again...", error);
-              }
-            );
+document.getElementById("sendEmail").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-          });
+  const serviceID = "service_yobjepi";
+  const templateID = "template_5gsql5o";
+
+  /* emailjs.sendForm(serviceID, templateID, this).then(
+    (response) => {
+      console.log("SUCCESS!", response.status, response.text);
+      alert("Email sent, thank you for your contact!");
+    },
+    (error) => {
+      console.log("FAILED...", error);
+      alert("Email failed, please try again...", error);
+    }
+  ); */
+
+  document.getElementById("authentication-modal").setAttribute('data-modal-hide', 'true');
+
+});
+
+/* document.getElementById("sendMail").addEventListener("submit", function(e){
+  e.preventDefault();
+  document.getElementById("authentication-modal").setAttribute('data-modal-hide', 'true');
+}) */
