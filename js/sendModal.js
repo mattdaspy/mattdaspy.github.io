d1056cb928461acaf55e80e0bcfcb97874f3c353
Matheus <64253249+mattdaspy@users.noreply.github.com>
2023-06-20 13:26:51 +0200

    changed position of setAttribute

--- js/sendModal.js
+++ js/sendModal.js
@@ -2,6 +2,7 @@ document
           .getElementById("sendEmail")
           .addEventListener("submit", function (event) {
             event.preventDefault();
+            document.getElementById("authentication-modal").setAttribute('data-modal-hide', 'true');
   
             const serviceID = "service_yobjepi";
             const templateID = "template_5gsql5o";
@@ -10,12 +11,10 @@ document
               (response) => {
                 console.log("SUCCESS!", response.status, response.text);
                 alert("Email sent, thank you for your contact!");
-                document.getElementById("authentication-modal").setAttribute('data-modal-hide', 'true');
               },
               (error) => {
                 console.log("FAILED...", error);
                 alert("Email failed, please try again...", error);
-                document.getElementById("authentication-modal").setAttribute('data-modal-hide', 'true');
               }
             );
 
