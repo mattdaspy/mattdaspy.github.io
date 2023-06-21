db29e33268c19d243dff75b2825b8ded4a6a8345
Matheus <64253249+mattdaspy@users.noreply.github.com>
2023-06-21 22:41:57 +0200

    Commented out form submition function to focus on project button now

--- js/sendModal.js
+++ js/sendModal.js
@@ -8,7 +8,7 @@ emailForm.addEventListener("submit", function (event) {
   const serviceID = "service_yobjepi";
   const templateID = "template_5gsql5o";
 
-  emailjs.sendForm(serviceID, templateID, this).then(
+  /* emailjs.sendForm(serviceID, templateID, this).then(
     (response) => {
       console.log("SUCCESS!", response.status, response.text);
       alert("Email sent, thank you for your contact!");
@@ -18,5 +18,5 @@ emailForm.addEventListener("submit", function (event) {
       console.log("FAILED...", error);
       alert("Email failed, please try again...", error);
     }
-  );
+  ); */
 });
