4fa565605b6936070f95c50acacc66559f6cda3a
Matheus <64253249+mattdaspy@users.noreply.github.com>
2023-06-21 20:55:10 +0200

    (test 8)

--- js/sendModal.js
+++ js/sendModal.js
@@ -1,6 +1,4 @@
 const modal = document.getElementById("authentication-modal")
-const closeModalButton = document.getElementById("closeModalButton")
-const sendEmailForm = document.getElementById("sendEmail")
 
 sendEmailForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -8,17 +6,17 @@ sendEmailForm.addEventListener("submit", (event) => {
   const serviceID = "service_yobjepi";
   const templateID = "template_5gsql5o";
 
-  /* emailjs.sendForm(serviceID, templateID, sendEmailForm).then(
+  emailjs.sendForm(serviceID, templateID, sendEmailForm).then(
     (response) => {
       console.log("SUCCESS!", response.status, response.text);
-      alert("Email sent, thank you for your contact!");
-      closeModal();
+        if (windows.confirm("Email sent, thank you for your contact!")){
+          settimeout(()=> { (modal.close(), 1000) } );
+        }
     },
     (error) => {
       console.log("FAILED...", error);
       alert("Email failed, please try again...", error);
     }
-  ); */
+  );
 
-  modal.click();
 });
